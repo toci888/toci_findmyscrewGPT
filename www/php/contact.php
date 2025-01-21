fa2f2f5a2d34be541abae01149f305ec1dbc774e
toci888 <bzapart@gmail.com>
2025-01-21 11:51:22 +0100

    security fix

--- www/php/contact.php
+++ www/php/contact.php
@@ -23,7 +23,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         $mail->Host = 'serwer2461761.home.pl'; // Wstaw adres serwera SMTP
         $mail->SMTPAuth = true;
         $mail->Username = 'warrior@toci.pl'; // Wstaw swój adres email
-        $mail->Password = 'ABY83beatka!@#'; // Wstaw swoje hasło
+        $mail->Password = ''; // Wstaw swoje hasło
         $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
         $mail->Port = 587;
 
